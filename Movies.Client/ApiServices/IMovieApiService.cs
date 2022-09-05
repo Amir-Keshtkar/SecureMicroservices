9be367b368140ac3383ecf2ba3c12f14fcf3a42a
Amirhossein Keshtkar <amir78ahkl@gmail.com>
2022-09-05 17:27:56 +0430

    build mvc client

---
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetById(string id);
+        Task<Movie> Create(Movie movie);
+        Task<Movie> Update(Movie movie);
+        Task Delete(string id);
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<Movie> Create(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movies=new List<Movie>();
+            movies.Add(new Movie()
+            {
+                Id=1,
+                Genre="Comics",
+                Title="asd",
+                ImageUrl="images/src",
+                Rating="9.2",
+                Owner="swn",
+                ReleaseDate=DateTime.Now,
+            });
+            return await Task.FromResult(movies);
+        }
+
+        public Task<Movie> Update(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
